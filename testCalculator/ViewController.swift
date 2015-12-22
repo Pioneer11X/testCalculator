679f70485ac91423b298df87ef301ce2522ed34b
Pioneer11X <sravan@mysmartprice.com>
2015-12-22 13:42:03 +0530

    Added the keypad functionality. And set up constraints for the display.

--- testCalculator/ViewController.swift
+++ testCalculator/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func numberPressed(sender: UIButton) {
+        // We need to get the value from the current label
+        // And then we need to update it with appending the pressed digit at the ending.
+        let digit = sender.currentTitle!
+        if ( userIsInTheMiddleOfTyping ){
+            display.text = display.text! + digit
+        } else{
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+        
+    }
+    
 
 }
 
