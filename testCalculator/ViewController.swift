bc4b57be602936037288b231154c0055de1a4016
Pioneer11X <sravan@mysmartprice.com>
2015-12-22 23:25:54 +0530

    Working copy without the layout constraints.

--- testCalculator/ViewController.swift
+++ testCalculator/ViewController.swift
@@ -36,6 +36,35 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            enter()
+        }
+        switch operation {
+        case "*": performOperation { $0 * $1 }
+        case "/": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "-": performOperation { $1 - $0 }
+        case "sqrt": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation( operation: (Double,Double)->Double){
+        if operandStack.count >= 2{
+            displayValue = operation(operandStack.removeLast() , operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation( operation: Double -> Double ){
+        if operandStack.count >= 1{
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
     var operandStack = Array<Double>()
     
     @IBAction func enter() {
